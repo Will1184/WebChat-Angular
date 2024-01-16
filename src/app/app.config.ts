144f6a3586da52d08b6131856deba2b3340b642f
Will1184 <brandonwilliammg@gmail.com>
2024-01-15 21:55:35 -0600

    feat: se agregaron  al proyecto primeNg para el diseño de la pagina; stomp,sockjs-client para el uso de websocket

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {provideAnimations} from "@angular/platform-browser/animations";
 import { routes } from './app.routes';
+import {FormsModule} from "@angular/forms";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),provideAnimations(),FormsModule]
 };
